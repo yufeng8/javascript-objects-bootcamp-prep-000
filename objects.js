15a62412605e1cbf6d218e4598bdaa9c5de85f85
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-11 02:07:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,9 @@
 var playlist = {"Phil Ochs": "Here's to the State of Mississippi"};
 
 function updatePlaylist(playlist, artist, title) {
-  return Object.assign({}, playlist, {artist: title});
+  var obj = {};
+  obj[artist] = title;
+  return Object.assign({}, playlist, obj);
 }
 
 function removeFromPlaylist(playlist, artist) {
