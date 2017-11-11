9365a4b8960b5b35abc90fcbaac910636d1af1a9
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-11 01:42:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {Caleb: "Hello"}
 
 function updatePlaylist(playlist, Yufeng, 'World') {
-  return object.assign({}, playlist, {Yufeng: 'World'})
+  return Object.assign({}, playlist, {Yufeng: 'World'})
 }
 
 function removeFromPlaylist(playlist, Yufeng) {
